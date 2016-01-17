441aa76581678864c68384296a130572a617ab02
pedecee <pedecee@gmail.com>
2016-01-17 22:35:34 +0000

    first test on part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,25 @@
 
 def sum arr
   # YOUR CODE HERE
+  # Define a method sum(array) that takes an array of integers as an argument 
+  # and returns the sum of its elements. For an empty array it should return zero.
+  return 0
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  # Define a method max_2_sum(array) which takes an array of integers as an argument 
+  # and returns the sum of its two largest elements. For an empty array it should return zero. 
+  # For an array with just one element, it should return that element.
+  return 0
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  # Define a method sum_to_n?(array, n) that takes an array of integers and an additional 
+  # integer, n, as arguments and returns true if any two elements in the array of integers 
+  # sum to n. An empty array should sum to zero by definition.
+  return 0
 end
 
 # Part 2
