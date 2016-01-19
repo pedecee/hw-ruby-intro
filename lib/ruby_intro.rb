bd554ddc30ce94cd60f8a7c3e683cb46e8fc2429
pedecee <pedecee@gmail.com>
2016-01-19 21:57:37 +0000

    added class part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -94,5 +94,47 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  # YOUR CODE HERE
+  # Define a class BookInStock which represents a book with an ISBN number, isbn, and price 
+  # of the book as a floating-point number, price, as attributes.
+
+  # The constructor should accept the ISBN number (a string, since in real life ISBN numbers 
+  # can begin with zero and can include hyphens) as the first argument and price as second argument, 
+  # and should raise ArgumentError (one of Ruby's built-in exception types) if the ISBN number is 
+  # the empty string or if the price is less than or equal to zero. 
+  # Include the proper getters and setters for these attributes.
+
+  # Include a method price_as_string that returns the price of the book formatted with a leading dollar sign 
+  # and two decimal places, that is, a price of 20 should format as "$20.00" and 
+  # a price of 33.8 should format as "$33.80".
+  
+  #class BookInStock
+    def initialize(isbn, price)
+      @isbn = isbn
+      @price = price
+    end
+    def isbn
+      @isbn
+    end
+    def isbn=(new_isbn)
+      @isbn = new_isbn
+    end
+    def price
+      @price
+    end
+    def price=(new_price)
+      @price = new_price
+    end
+    def price_as_string
+      price100 = 100 * price
+      price10 = 10 * price
+      if price100.modulo(100) == 0
+        "$""#{self.price}  .00"
+      elseif price10.modulo(10) == 0
+        "$#{self.price}.0"
+      else  
+        "$#{self.price}"
+      end  
+    end  
+  #end  
 end
